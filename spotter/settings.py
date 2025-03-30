88fd898382a1760bdabfda2a1d62885ffef8f8c3
Reinhardt Angira <reinhardtcollins@live.com>
2025-03-30 23:16:16 +0300

    chore: including url on origins

--- spotter/settings.py
+++ spotter/settings.py
@@ -67,13 +67,13 @@ MIDDLEWARE = [
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000', 
-    'https://spoter-frontend-n41lrvoa9-angira-collins-reinhardts-projects.vercel.app'
+    'https://spoter-frontend-7p6f8pwj9-angira-collins-reinhardts-projects.vercel.app'
 ]
 
 # Add CSRF trusted origins to allow POST requests from these domains
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000', 
-    'https://spoter-frontend-n41lrvoa9-angira-collins-reinhardts-projects.vercel.app'
+    'https://spoter-frontend-7p6f8pwj9-angira-collins-reinhardts-projects.vercel.app'
 ]
 
 ROOT_URLCONF = 'spotter.urls'
